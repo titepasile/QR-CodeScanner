424b3a2380435bc57dbf83ef25c65726baee6efa
Elena Tafelski <e.tafelski.inf21@stud.bbbaden.ch>
2023-11-23 11:13:37 +0100

    Titel

--- Calculator/App.js
+++ Calculator/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <header style={styles.header}>QR-Code Scanner</header>
+      <Text>Here is our super QR-Code Scanner.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8EACCD',
+    color: '#F9F3CC',
+    fontFamily: 'sans-serif',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    fontSize: '3rem',
+    padding: '60px',
+    fontFamily: 'cursive',
   },
 });
