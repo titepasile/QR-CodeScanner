a5c95bb017b78972213bf6b5a92a8e8b58d2385e
Elisa Petit-Sirigu <E.Petit.inf21@stud.bbbaden.ch>
2023-11-30 09:14:03 +0100

    added the screen navigation and scan button

--- Calculator/screens/Home.js
+++ Calculator/screens/Home.js
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Button, StyleSheet } from 'react-native'
 import React from 'react'
 
-const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+import { useNavigation } from '@react-navigation/native'
+
+export default function Home() {
+    const navigation = useNavigation();
+
+    return(
+        <View style={styles.container}>
+            <Button title='Scan' onPress={()=> navigation.navigate('Scanner')} />
+        </View>
+    )
 }
 
-export default Home
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
